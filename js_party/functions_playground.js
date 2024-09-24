31f367f21a9c67e08d43e22ae509b6ac41c398d6
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-24 17:21:58 -0400

    Completes all exercises

--- js_party/functions_playground.js
+++ js_party/functions_playground.js
@@ -85,27 +85,62 @@ watchShow()
 
 /*  --  SECTION B: FUNCTIONS WITH PARAMETERS. -- */
 //1B. Create a function takes in two numbers and logs the numbers added together. Invoke the function two times with different numbers.
+function addNums(num1, num2) {
+  console.log(`The sum of ${num1} + ${num2} is`, num1 + num2)
+}
 
+addNums(1, 2)
+addNums(3, 4)
 
 //2B. Create a function that takes in first name and last name and logs a greeting that greets somebody by first and last name. Call the function two times with different names.
+var firstLast = (firstName, lastName) => {
+  fulllName = (`${firstName} ${lastName}`)
+  return fulllName
+}
 
-
+var myFulllName = firstLast("Michael", "OBrien")
+console.log(myFulllName)
 
 /*  -- SECTION C: FUNCTIONS WITH RETURNS, ARGS and PARAMS -- */
 //1C. Make a function that takes in two numbers as arguments and returns the sum of those two numbers. 
 // Create a variable called 'total' and assign the variable's value to be an invocation of your function (dont forget to pass in 2 numbers!)  Console log 'total'
+var addNumbers = (num1, num2) => {
+  numsTotal = num1 + num2
+  return numsTotal
+}
 
+var total = addNumbers(4, 5)
+console.log(total)
+var total = addNumbers(6, 6)
+console.log(total)
 
 //2C. Make a function that takes in the name of a board game and a person's name and returns a string of "Do you want to play (board game name) with (persons name)?". Invoke the function with two different board game titles and names.
+var playGame = (game, name) => {
+  askToPlay = (`Do you want to play ${game} with ${name}?`)
+  return askToPlay
+}
 
-
+var wantToPlay = playGame("Monopoly", "Michael")
+console.log(wantToPlay)
+var wantToPlay = playGame("Cards Against Humanity", "Shollie")
+console.log(wantToPlay)
 
 //  *** CANT GET ENOUGH FUNCTION PRACTICE??  HERE'S SOME MORE!
 
 // 1D. Create a function called printInfo. This function should take in 3 arguments: `name`, `age` and `hometown`. The function should print a string that states your name, age and hometown using interpolation.
+var printInfo = (name, age, hometown) => {
+  usersInfo = (`Your name is ${name}.  You are ${age} years old, and from ${hometown}.`)
+  return usersInfo
+}
 
+var userInfo = printInfo("Michael", 57, "Nashville")
+console.log(userInfo)
 
 // 2D. Create a function called mathItUp. This function should take two parameters, both being numbers. The function should print the sum of the two numbers. It should also print a string that says `The value of the first parameter is ____ and value of the second parameter is _____.` The function should then return the product of these numbers + 100. 
+var mathItUp = (num1, num2) => {
+  mathedUp = (`The first parament is ${num1} and the second parameter is ${num2}. The product of these two numbers + 100 is`, num1 * num2 + 100)
+  return mathedUp
+}
 
-
-
+var mathFun = mathItUp(10, 9)
+console.log(mathFun)
