35b1deb96e05e874d2fc6f56ed2b27f226f840e2
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-07 15:11:41 -0400

    Fundamentals Updates

--- lessons/js_fundamentals.js
+++ lessons/js_fundamentals.js
@@ -27,20 +27,32 @@ let favoriteFoods = ['pizza', 'blueberries', 'ice cream', 'sushi']
 let partnerName = null;
 let jobTitle;
 
-// console.log(jobTitle)
+console.log(jobTitle)
 
+console.log(policeSketchDescription.eyes)
+console.log(policeSketchDescription.visibleTattoos)
+console.log(favoriteFoods[0])
+console.log(favoriteFoods[3])
 
+let quantity = 3;
+let mythicalCreature = " unicorns";
+let creatureCount = quantity + mythicalCreature
 
-
-
+console.log(quantity)
+console.log(mythicalCreature)
+console.log(creatureCount)
 
 /*
 JS Concatenation & Interpolation
 */ 
+let creatureCount2 = "<p>I have " + quantity + " very fancy" + mythicalCreature + "</p>"
+console.log(creatureCount2)
 
+let anotherQuantity = 3;
+let anotherMythicalCreature = "unicorns"; //notice no extra space in the "unicorns" string
+let anotherCreatureCount = `<p>I have ${quantity} very fancy ${mythicalCreature}</p>`
 
-
-
+console.log(anotherCreatureCount)
 
 
 
@@ -49,14 +61,36 @@ JS Concatenation & Interpolation
 
 /*
 JS Functions
-*/ 
+*/
+// ES5 function
+function logStatement() {
+  let myFirstStatement = "A varaible string called myFirstStatement"; 
+  // alert('This is my second statement!'); 
+  console.log(myFirstStatement); 
+  } 
+  
+  logStatement()
+// ES6 function
+  const logStatement2 = () => {
+    let myFirstStatement = "A variable string called myFirstStatement"; 
+    // alert('This is my second statement'); 
+    console.log(myFirstStatement);     
+    } 
 
+    logStatement2()
 
+let mathFtn = (num1, num2, num3) => {
+  let addNum = num1 +2;
+  let subNum = num2 -2;
+  let mulNum = num3 * 2;
+  let sumNums = num1 + num2 + num3;
+  console.log(`${num1} + 2 = ${addNum}`);
+  console.log(`${num2} - 2 = ${subNum}`);
+  console.log(`${num3} * 2 = ${mulNum}`);
+  console.log(`${num1} + ${num2} + ${num3} = ${sumNums}`);
+}
 
-
-
-
-
+mathFtn(2, 4, 6)
 /*
 JS Conditionals
 */ 
@@ -96,3 +130,24 @@ function evaluateSleep(hoursOfSleep) {
 JS For Loops
 */ 
 
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let points = [7, 3, 0, 14];
+
+function calculateTotalPoints() {
+  totalPoints = 0;
+  for (let i = 0; i < points.length; i++) {
+    totalPoints += points[i];
+  }
+  return totalPoints
+}
+
+console.log(calculateTotalPoints())
+
+function printTotalPoints() {
+  return `Your team scored ${calculateTotalPoints()} points!`;
+}
+
+console.log(printTotalPoints());
